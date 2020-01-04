5a6ac7c8c8ce5aa515a9988fb48b0be48d7f9ffd
bmalkmus <bmalkmus23@gmail.com>
2020-01-03 19:10:56 -0800

    first question displayed

---
+++ questions.js
@@ -0,0 +1,28 @@
+let questions = [
+    {
+        title: "Inside which HTML element do we place JavaScript?", 
+        choices: ["script tag", "title tag", "style tag", "body tag"],
+        answer: "script tag"
+
+    },
+    {
+        title: "Using a/an _____ statement is how you test for a specific condition", 
+        choices: ["If", "Select", "For", "forEach"],
+        answer: "If"
+    },
+    {
+        title: "What is meant by \"this\" keyword in Javascript?", 
+        choices: ["It refers to current object", "It refers to previous object", "It is a variable which contains value", "It is one of those filler words"],
+        answer: "It refers to current object"
+    },
+    {
+        title: "The ________ method of an Array objects adds and/or removes elements from an array.", 
+        choices: ["reverse", "shift", "slice", "splice"],
+        answer: "splice"
+    },
+    {
+        title: "A user encounters a webpage containing Javascript, what machine is actually executing the script?", 
+        choices: ["User's machine running a web browser", "Web Server", "CNS", "none of the above"],
+        answer: "User's machine running a web browser"
+    },
+]
---
+++ script.js
@@ -0,0 +1,67 @@
+// HTML Elements
+let start = document.getElementById("start-button");
+let quiz = document.getElementById("quiz");
+let timer = document.getElementById("timer");
+let counter = document.getElementById("counter");
+let questionTitle = document.getElementById("question-box");
+let options = document.getElementById("options");
+let ansOne = document.getElementById("one");
+let ansTwo = document.getElementById("two");
+let ansThree = document.getElementById("three");
+let ansFour = document.getElementById("four");
+
+
+
+// variables
+
+let questionLength = questions.length - 1;
+let currentQuestion = 0;
+let startTime = 0;
+let countdownStartTime= 15*questions.length
+
+// Start Quiz and counter
+counter.innerHTML = startTime
+start.addEventListener('click', function(){
+    presentQuestion();
+    start.style.display = "none";
+    let countDown = setInterval(function(){
+        counter.innerHTML = countdownStartTime;
+        countdownStartTime -= 1;
+        if (countdownStartTime < 0){
+            clearInterval(countDown);
+        }
+    }, 1000);
+    
+});
+
+
+
+
+
+// display Question
+
+function presentQuestion (){
+    let askedQuestion = questions[currentQuestion];
+    
+
+    questionTitle.innerHTML =askedQuestion.title;
+
+    ansOne.innerHTML = askedQuestion.choices[0];
+    ansTwo.innerHTML = askedQuestion.choices[1];
+    ansThree.innerHTML = askedQuestion.choices[2];
+    ansFour.innerHTML = askedQuestion.choices[3];
+
+    start.style.display = "none";
+    quiz.style.display = "block";
+    
+};
+
+// start.style.display = "none";
+// quiz.style.display = "block";
+
+
+
+
+
+
+
